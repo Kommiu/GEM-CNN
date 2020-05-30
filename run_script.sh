4a1aaa4e431ca9930fed654ce1cc8c7475f5ce0a
Roman <roman.misiutin@gmail.com>
2020-05-30 16:30:40 +0300

    made universal bashscript for running train and test scripts in docker

---
+++ run_script.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+#export DATA_DIR=$(pwd)/data/geom_dataset
+while getopts c:d: flag
+do
+  case "${flag}" in
+    c) config_file=${OPTARG};;
+    d) data_root=${OPTARG};;
+  esac
+done
+script=${@:$OPTIND:1}
+docker run \
+  --gpus all \
+  --rm \
+  --shm-size=32G \
+  --user kommiu \
+  -v $(pwd):/home/kommiu/app \
+  -v $data_root:/data \
+  gem-cnn \
+  python $script --config $config_file
